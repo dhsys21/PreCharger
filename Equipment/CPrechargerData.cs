8b03ac9193c05971f13601185eda8af250e701c0
dhsys21 <smgang@dhsys21.com>
2023-04-12 15:15:06 +0900

    GetMeasure Values 추가5

--- Equipment/CPrechargerData.cs
+++ Equipment/CPrechargerData.cs
@@ -361,14 +361,14 @@ namespace PreCharger
         {
             for(int cIndex = 0; cIndex < 256; cIndex++)
             {
-                _sVolt[cIndex] = (voltages[cIndex] * 1000.0).ToString("F1");
+                _sVolt[cIndex] = (voltages[cIndex] * 1000.0).ToString("F2");
             }
         }
         public void SetCurrents(double[] currents)
         {
             for (int cIndex = 0; cIndex < 256; cIndex++)
             {
-                _sCurr[cIndex] = (currents[cIndex] * 1000.0).ToString("F2");
+                _sCurr[cIndex] = (currents[cIndex] * 1000.0).ToString("F1");
             }
         }
 
